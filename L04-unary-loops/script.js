aa140442e383557c4df4159f082f7a6ecb5bbf18
Tarun Pandey <tpandey495@gmail.com>
2026-07-16 12:42:31 +0530

    loops

---
+++ L04-unary-loops/script.js
@@ -0,0 +1,96 @@
+// unary operator -> works on single operand
+// given a variable a increment it by 1
+// using assignment
+// let a =10;
+// a=a+1;
+// console.log(a); //11
+
+// using compound assignment
+// let a =10;
+// a+=1;
+// console.log(a);
+
+// using post increment
+// let a =10;
+// a++;
+// console.log(a); //11
+
+// let  b=10;
+// console.log(b++); //10
+
+
+// let a = 10;
+// console.log(a+a++); // 20
+
+// let b = 10;
+// b++;
+// console.log(b++); //11
+// console.log(b);  //12
+
+// o/p->
+// let  a;
+// a++;
+// console.log(a); //NaN
+
+// let a;
+// a++;
+// a=0;
+// console.log(a); //0
+
+// decrement a number by 1
+// let b =10;
+// b=b-1;
+// console.log(b); //9
+
+// let b=10;
+// b-=1;
+// console.log(b);
+
+// post decrement
+// let b=10;
+// console.log(b--); //10
+// console.log(b); //9
+
+// let b =9;
+// b--;
+// console.log(b++); //8
+// console.log(b); //9
+
+// let a =5,b=9;
+// console.log(a+++b+b--); // 23
+
+
+// let a = 2 ,b= 4;
+// a++;
+// console.log(a+b); //7
+
+
+// pre increment and pre decrement
+// let a =10;
+// console.log(++a); //11
+
+// let b =9;
+// console.log(--b); //8
+
+// let a = 8;
+// let b = 2; 
+// console.log(++a+b--) // 11
+
+// loop
+// print hello world 5 times
+// console.log("hello world");
+// console.log("hello world");
+// console.log("hello world");
+// console.log("hello world");
+// console.log("hello world");
+
+// for loop
+for(let i =1; i<= 5;i++){
+    console.log("Hello world");
+}
+
+// intialization
+// condition
+// code 
+// update 
+
